18d2a106110dd70cdfc88af9f323a9f0825b5469
Ivan Angelov <kontakta39@gmail.com>
2024-03-06 21:37:30 +0200

    Update sixth exercise solution

--- 03-Functions and Statements-Lab-Tasks/06. Sign Check/script.js
+++ 03-Functions and Statements-Lab-Tasks/06. Sign Check/script.js
@@ -1,5 +1,16 @@
-function solve() {
+function solve(numberOne, numberTwo, numberThree) {
+    let result = mathMultiply(mathMultiply(numberOne, numberTwo), numberThree);
 
+    function mathMultiply(a, b) {
+        let operation = (a, b) => a * b; 
+        return operation(a, b);
+    }
+
+    if (result > 0) {
+        console.log(`Positive`);
+    } else {
+        console.log(`Negative`);
+    }
 }
 
 solve( 5, 12, -15);
