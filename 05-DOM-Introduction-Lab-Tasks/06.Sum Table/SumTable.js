126590081ec373345b764170fdc958705908b875
Ivan Angelov <kontakta39@gmail.com>
2024-03-20 23:38:59 +0200

    Updated solutions

--- 05-DOM-Introduction-Lab-Tasks/06.Sum Table/SumTable.js
+++ 05-DOM-Introduction-Lab-Tasks/06.Sum Table/SumTable.js
@@ -1,3 +1,19 @@
 function sumTable() {
+    let sum = 0;
+    let rows = document.querySelectorAll("table tbody tr");
 
+    for (let row of rows) {
+        let cells = row.getElementsByTagName('td');
+
+        for (let item of cells) {
+            let currentElement = Number(item.textContent);
+
+            if (!isNaN(currentElement)) {
+                sum += currentElement
+            }
+        }
+    }
+
+    let sumElement = document.getElementById("sum");
+    sumElement.textContent = sum;
 }
--- 05-DOM-Introduction-Lab-Tasks/07.Extract Parenthesis/ExtractParenthesis.js
+++ 05-DOM-Introduction-Lab-Tasks/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,16 @@
 function extract(content) {
+  let text = document.getElementById(content);
+  let matches = text.textContent.match(/\((.*?)\)/g);
+  let textArray = matches.map(match => match.slice(1, -1));
+  let finalString = '';
 
+  for (let i = 0; i < textArray.length; i++) {
+    if (i === textArray.length - 1) {
+        finalString += textArray[i];
+    } else {
+        finalString += `${textArray[i]}; `;
+    }
+  }
+
+  return finalString;
 }
